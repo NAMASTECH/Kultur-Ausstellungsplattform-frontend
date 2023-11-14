b154cc5d1504833a7e1a6357f2997d690eb85333
Andreas Ohligschläger <andreas_ohl_t@hotmail.com>
2023-11-14 23:39:03 +0100

    added key to card

--- src/js/components/event/EventCard.jsx
+++ src/js/components/event/EventCard.jsx
@@ -1,7 +1,7 @@
 
-export default function EventCard({event: { eventTitle, artist, eventType, eventCategory, img, description, homepage, dateStart, dateEnd, timeStart, timeEnd, venueName, venueType, venues }}) {
+export default function EventCard( {event: { _id, eventTitle, artist, eventType, eventCategory, img, description, homepage, dateStart, dateEnd, timeStart, timeEnd, venueName, venueType, venues }}) {
     return (
-        <div className="Card" style={{ width: '15em', background: 'white', border: '1px #DDE1E6 solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex', margin: "15px", minHeight: "100%" }}>
+        <div key={_id} className="Card" style={{ width: '15em', background: 'white', border: '1px #DDE1E6 solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex', margin: "15px", minHeight: "100%" }}>
             <img className="PlaceholderPicture" style={{ alignSelf: 'stretch', width: "100%", minHeight: "50px", background: 'linear-gradient(0deg, #DDE1E6 0%, #DDE1E6 100%)' }} src={img} />
             <div className="Content" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start', alignSelf: 'stretch', paddingTop: 24, paddingBottom: 16, paddingLeft: 16, paddingRight: 16, gap: 16 }}>
                 <div className="artists" style={{ fontSize: 20, fontWeight: '700', }}>{artist} </div>
