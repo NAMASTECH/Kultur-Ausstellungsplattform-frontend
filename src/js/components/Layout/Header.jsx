3d2733ea25cd2e7415174d588feccad3a0ac5fbc
Andreas Ohligschläger <andreas_ohl_t@hotmail.com>
2023-11-14 22:43:39 +0100

    minor refactoring

--- src/js/components/Layout/Header.jsx
+++ src/js/components/Layout/Header.jsx
@@ -10,24 +10,16 @@ function Header() {
         <NavLink to="/">Home</NavLink>
         {/* {!isOnline() && <NavLink to="/register">Sign Up</NavLink>} */}
         {!isOnline() && <NavLink to="/register/user">Sign Up user</NavLink>}
-        {!isOnline() && (
-          <NavLink to="/register/organizer">Sign Up organization</NavLink>
-        )}
-        {/*isOnline() && */ <NavLink to="/event">Add Event</NavLink>}
-        {!isOnline() && <NavLink to="/events">Events</NavLink>}
+        {!isOnline() && (<NavLink to="/register/organizer">Sign Up organization</NavLink>)}
+        {<NavLink to="/event">Add Event</NavLink>}
+        {<NavLink to="/events">Events</NavLink>}
         {isOnline() && <NavLink to={`/users/${userData.id}`}>My Data</NavLink>}
         {isOnline() && <NavLink to="/users">Users</NavLink>}
         {!isOnline() && <NavLink to="/login">Log In</NavLink>}
         {isOnline() && <NavLink to="/logout">Sign Out</NavLink>}
-
-        {isOnline() && <h3>Hallo, {userData.username}!</h3>}
-
-        {/* {
-          <NavLink to="/api/EventDisplayAndSearch">
-            EventDisplayAndSearch
-          </NavLink>
-        } */}
       </nav>
+      <br />
+      {isOnline() && <h3>Hallo, {userData.username}!</h3>}
     </header>
   );
 }
